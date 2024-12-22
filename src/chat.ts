87cb660bf0e59284d10966f3f7601c132aea43aa
Miguel <expeliam02@hotmail.com>
2024-12-22 10:54:51 -0700

    Checkpoint - I am able to use rooms.

--- src/chat.ts
+++ src/chat.ts
@@ -1,9 +1,17 @@
-console.log("Hello from TS 14!");
+console.log("v.20!");
 import { io } from "socket.io-client";
 import Mustache from "mustache";
+import notMyCircusNotMyMonkeys from "moment";
+import queryString from 'query-string';
 
-const socket = io();
 
+const socket = io("http://localhost:3000");
+// window.socket? = socket; 
+// const socket = io();
+
+//$ This would be the same as const socket = io() without the url since It will take whatever the URL it is that is in the browser window;
+
+//*Elements:
 const $messageForm = document.querySelector("#message-form");
 const $messageFormInput = $messageForm!.querySelector("input");
 const $messageFormButton = $messageForm?.querySelector("button");
@@ -12,10 +20,28 @@ const $messages = document.querySelector("#messages");
 
 //* Templates
 const $messageTemplate = document.querySelector("#message-template")!.innerHTML;
+const $locationTemplate = document.querySelector("#location-template")!.innerHTML;
+
+//*Options:
+const { username, room } = queryString.parse(location.search)
+
+//*Recievers (on()'s):
+socket.on("connect", () => {
+	console.log("Connected to server!!!");
+});
 
 socket.on("mainMessage", (msg) => {
 	console.log(msg);
-	const html = Mustache.render($messageTemplate, { message: msg });
+	const html = Mustache.render($messageTemplate, {
+		message: msg.text,
+		createdAt: notMyCircusNotMyMonkeys(msg.createdAt).format("h:mm a"),
+	});
+	$messages!.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("location", (msg) => {
+	console.log(msg);
+	const html = Mustache.render($locationTemplate, { location: msg.url, createdAt: notMyCircusNotMyMonkeys(msg.createdAt).format("h:mm a") });
 	$messages!.insertAdjacentHTML("beforeend", html);
 });
 
@@ -25,24 +51,20 @@ $messageForm!.addEventListener("submit", (e) => {
 	e.preventDefault();
 	$messageFormButton?.setAttribute("disabled", "disabled");
 
-	const input: any = (e.target as any).elements.message!.value;
+	//$ I just believe the wait to do it below is harder:
+	// const input: any = (e.target as any).elements.message!.value;
 	//$e = event, target = form element, elements = all form elements, message = name of the element (name="message")
+	const input: any = $messageFormInput!.value;
+
 	$messageFormInput!.value = "";
 	$messageFormInput!.focus();
 
-	socket.emit("sendMessage", input, (e: any) => {
+	socket.emit("sendMessage", input, (cb: any) => {
 		//! enable
 		$messageFormButton!.removeAttribute("disabled");
-		if (e) {
-			return console.log(e);
+		if (cb) {
+			return console.log(cb);
 		}
-		console.log("Message delivered!");
-
-		// console.log(cbConfirmantion);
-	});
-
-	socket.emit("eventName", "Hello", (acknoMsg: any) => {
-		console.log(acknoMsg);
 	});
 });
 
@@ -73,3 +95,7 @@ $sendLocationButton!.addEventListener("click", () => {
 		}
 	);
 });
+
+
+
+socket.emit('join', { username, room })
