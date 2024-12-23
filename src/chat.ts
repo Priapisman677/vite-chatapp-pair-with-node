4ebda7bdef2a68a493eb7cf5eb2357dab9f67c66
Miguel <expeliam02@hotmail.com>
2024-12-22 20:24:49 -0700

    server working- Before working with test fixing

--- src/chat.ts
+++ src/chat.ts
@@ -1,12 +1,11 @@
-console.log("v.20!");
+console.log("v.21!");
 import { io } from "socket.io-client";
 import Mustache from "mustache";
-import notMyCircusNotMyMonkeys from "moment";
-import queryString from 'query-string';
-
+import Moment from "moment";
+import queryString from "query-string";
 
 const socket = io("http://localhost:3000");
-// window.socket? = socket; 
+// window.socket? = socket;
 // const socket = io();
 
 //$ This would be the same as const socket = io() without the url since It will take whatever the URL it is that is in the browser window;
@@ -20,10 +19,11 @@ const $messages = document.querySelector("#messages");
 
 //* Templates
 const $messageTemplate = document.querySelector("#message-template")!.innerHTML;
-const $locationTemplate = document.querySelector("#location-template")!.innerHTML;
+const $locationTemplate =
+	document.querySelector("#location-template")!.innerHTML;
 
 //*Options:
-const { username, room } = queryString.parse(location.search)
+const { username, room } = queryString.parse(location.search);
 
 //*Recievers (on()'s):
 socket.on("connect", () => {
@@ -33,15 +33,20 @@ socket.on("connect", () => {
 socket.on("mainMessage", (msg) => {
 	console.log(msg);
 	const html = Mustache.render($messageTemplate, {
+		username: msg.username,
 		message: msg.text,
-		createdAt: notMyCircusNotMyMonkeys(msg.createdAt).format("h:mm a"),
+		createdAt: Moment(msg.createdAt).format("h:mm a"),
 	});
 	$messages!.insertAdjacentHTML("beforeend", html);
 });
 
 socket.on("location", (msg) => {
 	console.log(msg);
-	const html = Mustache.render($locationTemplate, { location: msg.url, createdAt: notMyCircusNotMyMonkeys(msg.createdAt).format("h:mm a") });
+	const html = Mustache.render($locationTemplate, {
+		username: msg.username,
+		location: msg.url,
+		createdAt: Moment(msg.createdAt).format("h:mm a"),
+	});
 	$messages!.insertAdjacentHTML("beforeend", html);
 });
 
@@ -96,6 +101,9 @@ $sendLocationButton!.addEventListener("click", () => {
 	);
 });
 
-
-
-socket.emit('join', { username, room })
+socket.emit("join", { username, room }, (cb: any) => {
+	if (cb) {
+		alert(cb);
+		location.href = "/";
+	}
+});
